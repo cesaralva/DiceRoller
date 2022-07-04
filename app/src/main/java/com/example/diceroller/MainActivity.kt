161a446fc2d4be46f20e2799fc73545ecbbb192f
Cesar Alva <csarx09@gmail.com>
2022-07-04 00:04:08 -0500

    nuevocomentado

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -7,26 +7,32 @@ import android.widget.TextView
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         val rollButton: Button = findViewById(R.id.roll_button)
         rollButton.setOnClickListener { rollDice() }
-
     }
-
     private fun rollDice() {
+        // Toast.makeText(this, "button clicked",
+        //   Toast.LENGTH_SHORT).show()
         val randomInt = (1..6).random()
-        //Toast.makeText(this, "button clicked",
-            //Toast.LENGTH_SHORT).show()
         val resultText: TextView = findViewById(R.id.result_text)
-        //resultText.text = "Dice Rolled!"
         resultText.text = randomInt.toString()
-
-
-
     }
-
-
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
